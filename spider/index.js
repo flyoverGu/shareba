48fa1358bf9e54930be6fc5fe6e5fd4678b7c047
flyover <flyover.gu@gmail.com>
2015-10-20 22:41:55 +0800

    爬虫文章基本完成了

--- spider/index.js
+++ spider/index.js
@@ -2,19 +2,35 @@ var co = require('co');
 var movie = require('./movie');
 var Post = require('../dao/post');
 var config = require('../config.json');
-var mongo = require('koa-mongo');
+var mdb = require('../dao/mongodb');
 
-co(function* () {
-    yield mongo(config)(function *() {
-        console.log(this);
-    });
-    let data = yield movie();
-    let res = yield Post.search(m, data.title);
-    if (res && res.length) {
-    } else {
-        yield Post.save(m, {id: 'admin'}, data);
-    }
+// 爬取间隔时间
+let time = 1000 * 60 * 12;
 
-}).catch(function(err) {
-    console.log(err);
-})
+
+let start = () => {
+    co(function*() {
+        
+
+        let m =
+            yield mdb(config.mongo);
+
+        let data =
+            yield movie();
+
+        let res =
+            yield Post.search(m, data.title);
+
+        console.log(res);
+        // 避免重复查询
+        if (res && res.length) {} else {
+            yield Post.save(m, {
+                id: 'admin'
+            }, data);
+        }
+    }).catch(function(err) {
+        console.log(err);
+    })
+}
+
+setInterval(() => start(), time);
