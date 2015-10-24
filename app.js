062c64a5e99895cb121a7bccf0906be6a2569106
flyover <flyover.gu@gmail.com>
2015-10-24 15:26:14 +0800

    add wechat verify

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 let koa = require('koa');
 let bodyparser = require('koa-bodyparser');
 let route = require('koa-route');
+let sha1 = require('sha1');
 
 let app = koa();
 app.use(bodyparser());
 
 
-app.use(function* (next)  {
+app.use(function*(next) {
     try {
         yield next;
-    } catch(e) {
+    } catch (e) {
         console.log(e);
     }
 });
 
 let api = "/api/v1/";
 app.use(route.get(api + "wechat/verify", function*() {
+    let {signature, timestamp, nonce, echostr} = this.query;
     console.log(this.query);
+    if (verifyWechat(signature, timestamp, nonce)) {
+        this.body = echostr; 
+    } else {
+        this.body = "fail";
+    }
 }));
 
-let port = 8080;
-app.listen(port, ()=> console.log("start !!!"));
+let verifyWechat = (signature, timestamp, nonce) => {
+    if (!signature || !timestamp || !nonce) {
+        return false;
+    }
+    var arr = [token, timestamp, nonce];
+    arr.sort();
+    var str = arr.join('');
+    return signature == sha1(str);
+}
 
+let port = 8080;
+app.listen(port, () => console.log("start !!!"));
