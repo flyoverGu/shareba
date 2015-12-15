f60e9b6971d2b3abd7efd2ddac471ffbd96c0b91
flyover <flyover.gu@gmail.com>
2015-12-15 23:16:33 +0800

    更新爬取数据

--- spider/movie.js
+++ spider/movie.js
@@ -1,10 +1,10 @@
 'use strict';
 
-
 let http = require('superagent');
 let $ = require('cheerio');
 let co = require('co');
 let db = require('./db');
+let _ = require('underscore');
 
 // 网络获取
 let getServerMovie = (url, parse) => new Promise((resolve, reject) =>
@@ -15,7 +15,6 @@ let getServerMovie = (url, parse) => new Promise((resolve, reject) =>
 // 豆瓣电影解析器
 let parseDoubanMovie = (html) => {
     return commonParse(html, '#nowplaying ul.lists>li', (el, res) => res.push({
-        id: 'douban',
         title: $(el).data('title'),
         count: $(el).data('votecount'),
         score: $(el).data('score'),
@@ -27,18 +26,19 @@ let parseDoubanMovie = (html) => {
 // 美团解析器
 let parseMeituanMovie = (html) => {
     return commonParse(html, '.movies-container .movie-cell', (el, res) => res.push({
-        id: 'meituan',
         title: $(el).find('.movie-cell__cover').attr('title'),
         score: parseInt($(el).find('.rate-stars').attr('style').replace('width:', '')) / 10
     }));
 }
 
 let parseTaobaoMoive = (html) => {
-    return commonParse(html, '.center-wrap .tab-content>.tab-movie-list .movie-card-wrap', (el, res) => res.push({
-        id: 'taobao',
-        title: $(el).find('.movie-card-name .bt-l').html(),
-        score: $(el).find('.movie-card-name .bt-r').html()
-    }));
+    // TODO 这里选择器有问题，有重复数据
+    return commonParse(html, '.center-wrap .tab-content>.tab-movie-list .movie-card-wrap', (el, res) => {
+        res.push({
+            title: $(el).find('.movie-card-name .bt-l')[0].children[0].data,
+            score: $(el).find('.movie-card-name .bt-r').html()
+        })
+    });
 }
 
 let commonParse = (html, selector, dataFun) => {
@@ -58,7 +58,7 @@ let commonParse = (html, selector, dataFun) => {
 
 let start = (list, ser, cb) => {
     co(function*() {
-        return yield list.map((item) => ser(item.url, item.parse));
+        return yield _.mapObject(list, (item) => ser(item.url, item.parse));
     }).then((val) => cb(null, val), (err) => cb(err));
 }
 
@@ -69,16 +69,20 @@ let handleData = (val) => {
 
 let thinkF = (list, ser) => (cb) => start(list, ser, cb);
 
-const spiderList = [{
-    url: 'https://dianying.taobao.com/showList.htm',
-    parse: parseTaobaoMoive
-}, {
-    url: 'http://movie.douban.com/nowplaying/hangzhou/',
-    parse: parseDoubanMovie
-}, {
-    url: 'http://hz.meituan.com/dianying/zuixindianying',
-    parse: parseMeituanMovie
-}]
+const spiderList = {
+    'taobao': {
+        url: 'https://dianying.taobao.com/showList.htm',
+        parse: parseTaobaoMoive
+    },
+    //'douban': {
+    //    url: 'http://movie.douban.com/nowplaying/hangzhou/',
+    //    parse: parseDoubanMovie
+    //},
+    //'meituan': {
+    //    url: 'http://hz.meituan.com/dianying/zuixindianying',
+    //    parse: parseMeituanMovie
+    //}
+}
 
 // 对外启动接口
 module.exports = () => thinkF(spiderList, getServerMovie)((err, val) => handleData(val));
